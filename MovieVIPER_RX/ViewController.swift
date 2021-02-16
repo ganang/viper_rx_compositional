3cd9b1e3b6b276b442a592c578ccfba7eb1031e9
Ganang Arief Pratama <ganang.arief.p@gmail.com>
2021-02-16 12:40:16 +0700

    Initial Commit

---
+++ MovieVIPER_RX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieVIPER_RX
+//
+//  Created by Ganang Arief Pratama on 16/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
